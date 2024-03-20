8196f41da6afd2b5a146b75bca5d4e03212cb146
Kakon Debnath <kakondebnath96@gmail.com>
2024-03-20 22:54:32 +0600

    navbar Done

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -1,10 +1,54 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import Container from '../Container/Container';
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    );
+  return (
+    <div className="py-4 flex justify-between items-center">
+      <Link href="/">
+        <h2 className="text-3xl font-semibold">
+          Kabbo <span className="text-orange-500">Baby</span> Shop
+        </h2>
+      </Link>
+      <div>
+        <Link href="/">
+          <Button className="text-lg" variant="ghost">
+            Home
+          </Button>
+        </Link>
+        <Link href="/">
+          <Button className="text-lg" variant="ghost">
+            Category
+          </Button>
+        </Link>
+        <Link href="/">
+          <Button className="text-lg" variant="ghost">
+            Products
+          </Button>
+        </Link>
+        <Link href="/">
+          <Button className="text-lg" variant="ghost">
+            Flash Sale
+          </Button>
+        </Link>
+        <Link href="/">
+          <Button className="text-lg" variant="ghost">
+            About Us
+          </Button>
+        </Link>
+        <Link href="/">
+          <Button className="text-lg" variant="ghost">
+            Contact Us
+          </Button>
+        </Link>
+        <Link href="/dashboard">
+          <Button className="text-lg" variant="ghost">
+            Dashboard
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
